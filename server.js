5e8b3466074067f1b8c13ac6e23d0743d442b2de
Michael Wanyoike <micwanyoike@gmail.com>
2020-04-15 18:55:29 +0300

    Implemented metrics with node-exporter-prometheus

--- server.js
+++ server.js
@@ -1,37 +1,26 @@
 // server.js
 const jsonServer = require('json-server');
-const server = jsonServer.create();
+const app = jsonServer.create();
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-const promClient = require('prom-client');
+const middlewares = jsonServer.defaults()
 
-const Counter = promClient.Counter;
-// create http request counter
-const c = new Counter({
-  name: 'http_reqs',
-  help: 'number of http requests',
-});
-server.use((req, res, next) => {
-  c.inc();  // increment the request counter
-  next() // continue to JSON Server router
-});
+// Collect metrics
+const prometheusExporter = require('@tailorbrands/node-exporter-prometheus');
+const options = {
+  appName: "crocodile-api",
+  collectDefaultMetrics: true,
+  ignoredRoutes: ['/metrics', '/favicon.ico', '/__rules']
+};
+const promExporter = prometheusExporter(options);
+app.use(promExporter.middleware);
+app.get('/metrics', promExporter.metrics);
 
-// const collectDefaultMetrics = promClient.collectDefaultMetrics;
-// const Registry = promClient.Registry;
-// const register = new Registry();
-// collectDefaultMetrics({ register });
+app.use(middlewares);
+app.use(router);
 
-
-// server.get('/metrics', (req, res) => {
-//   res.set('Content-Type', register.contentType);
-//   res.end(register.metrics());
-// });
-
-server.use(middlewares);
-server.use(router);
 const port = process.env.PORT || 3000;
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(
-    `Server listening to http://localhost:${port}, metrics exposed on /metrics endpoint`,
+    `JSON server listening on 127.0.0.1:${port}`,
   );
 });
